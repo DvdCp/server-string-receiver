5729050936781d54b7f2edb07fa0535d1e7ddea7
DavidC <dav.capuano98@gmail.com>
2023-08-22 12:40:27 +0200

    server.js first implementation

---
+++ server.js
@@ -0,0 +1,33 @@
+const net = require('net');
+const axios = require('axios');
+
+const server = net.createServer(socket => {
+  console.log('Client connected');
+
+  socket.on('data', data => {
+      const receivedString = data.toString();
+      console.log(`Received from client: ${receivedString}`);
+      
+      // Send received string to a endpoint through POST
+      axios.post('http://127.0.0.1:5000/submitString', receivedString, 
+        { headers: { 
+          'Content-Type': 'application/json',
+        }
+      })
+      
+  });
+
+  socket.on('end', () => {
+      console.log('Client disconnected');
+  });
+
+  socket.on('error', err => {
+      console.error(`Socket error: ${err.message}`);
+  });
+});
+
+const serverPort = 3000;
+
+server.listen(serverPort, () => {
+  console.log(`Server listening on port ${serverPort}`);
+});
