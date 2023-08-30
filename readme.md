954b76b5c04dfde2fb014b583d7d0d5e46ec7817
DavidC <dav.capuano98@gmail.com>
2023-08-30 19:54:15 +0200

    first readme.md & using service name as host

---
+++ readme.md
@@ -0,0 +1,1 @@
+docker run --name server-string-receiver --network myNet server-string-receiver
--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const net = require('net');
-const axios = require('axios');
+const net = require("net");
+const axios = require("axios");
 
-const server = net.createServer(socket => {
-  console.log('Client connected');
+const server = net.createServer((socket) => {
+  console.log("Client connected");
 
-  socket.on('data', data => {
-      const receivedString = data.toString();
-      console.log(`Received from client: ${receivedString}`);
-      
-      // Send received string to a endpoint through POST
-      axios.post('http://127.0.0.1:5000/submitString', receivedString, 
-        { headers: { 
-          'Content-Type': 'application/json',
-        }
-      })
-      
+  socket.on("data", (data) => {
+    const receivedString = data.toString();
+    console.log(`Received from client: ${receivedString}`);
+
+    // Send received string to a endpoint through POST
+    axios.post("http://python-web-service:5000/submitString", receivedString, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   });
 
-  socket.on('end', () => {
-      console.log('Client disconnected');
+  socket.on("end", () => {
+    console.log("Client disconnected");
   });
 
-  socket.on('error', err => {
-      console.error(`Socket error: ${err.message}`);
+  socket.on("error", (err) => {
+    console.error(`Socket error: ${err.message}`);
   });
 });
 
